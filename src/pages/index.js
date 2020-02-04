4a0a3f520e5e8f7732d0ca7a7321d8f1c094a32e
Ben Kitching <kitchingben@gmail.com>
2020-02-04 22:07:38 +0000

    components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
-
-export default () => <div>Hello world! Floorz</div>
+import Header from "../components/header"
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header />
+    <p>Such wow. Very React.</p>
+  </div>
+)
+export default () => <div>Hello world! Floorz s s s</div>
